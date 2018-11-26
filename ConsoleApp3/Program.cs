8da00a8d92113941c54fdd1119c150fa40572e27
昱廷 陳 <popew800i@hotmail.com>
2018-11-26 10:08:26 +0800

    加入專案檔案。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] scores = { 1, 2, 3, 4, 5, 60, 61, 64, 45, 87, 100, 21, 56 };
+            var result =
+                from c in scores
+                where c > 80
+                select c;
+
+            result.ForEach(x => { Console.WriteLine(x.ToString()); });
+
+
+        }
+    }
+}
