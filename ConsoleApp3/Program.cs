61a57d2cddcc48501aae3b62e85ca28ca16c1eee
昱廷 陳 <popew800i@hotmail.com>
2018-11-26 21:56:12 +0800

    update

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -14,11 +14,14 @@ namespace ConsoleApp3
             int[] scores = { 1, 2, 3, 4, 5, 60, 61, 64, 45, 87, 100, 21, 56 };
             var result =
                 from c in scores
-                where c > 80
+                where c > 0
                 select c;
 
-            result.ForEach(x => { Console.WriteLine(x.ToString()); });
+            var results =
+                scores.ToList().Where(c => c < 10).Select(c);
 
+            result.ToList().ForEach(x => { Console.WriteLine(x.ToString()); });
+            Console.ReadKey();
 
         }
     }
